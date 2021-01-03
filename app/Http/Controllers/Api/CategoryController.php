15d07dd9539c3db32b74fa0271acb74f15010d49
Oscar Catari Gutierrez <oscarcatari@outlook.es>
2021-01-03 19:15:43 +0000

    [ADD] Category API. Get all, get, delete and create

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        // Base url
+        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+            $url = "https://";
+        else
+            $url = "http://";
+        // Append the host(domain name, ip) to the URL.
+        $url.= $_SERVER['HTTP_HOST'];
+
+        // Add url to images
+        foreach ($categories as $category)
+            $category->image = $url . "/storage/category/" . $category->image;
+
+        return response()->json($categories);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $category = new Category();
+
+        $path = $request->file('image')->store('category');
+
+        $category->name = $request->name;
+        // Take generated image name
+        $category->image = substr($path, strrpos($path, '/') + 1);
+        $category->save();
+
+        return response()->json($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $category = Category::where('id', $id)->first();
+
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+
+        return response()->json($category);
+    }
+}
